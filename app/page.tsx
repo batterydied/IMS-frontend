d41fd3ee7acc95bb5a518d7ae7f5e61d6a6b3783
Benson Zheng <bensonzheng2003@gmail.com>
2025-10-27 01:36:47 -0400

    updated readme.md and UI for Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 "use client"
 import { useSupabase } from "@/contexts/SupabaseProvider"
 import { useRouter } from "next/navigation"
-import { useEffect } from "react"
+import { useCallback, useEffect } from "react"
 import Navbar from "@/components/Navbar"
 
 export default function App() {
   const router = useRouter()
   const { user, supabase, isLoading } = useSupabase()
 
-  const handleLogOut = async () => {
+  const handleLogOut = useCallback(async () => {
     await supabase.auth.signOut()
     router.replace("/auth/signin")
-  }
+  }, [router, supabase.auth])
 
   useEffect(() => {
     if (!isLoading && !user) {
@@ -26,13 +26,13 @@ export default function App() {
   )
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <Navbar handleLogOut={()=>{}} handleUpload={()=>{}}/>
+    <div className="flex flex-col items-center justify-center h-screen bg-primary">
+      <Navbar handleLogOut={handleLogOut} handleUpload={()=>{}}/>
       <div className="grid grid-cols-[35%_1fr] gap-2 p-6 h-full w-full">
-        <div className="bg-white min-h-[50px]">
-
+        <div className="bg-secondary p-2">
+          <text className="text-content">Welcome! Glad to see you.</text>
         </div>
-        <div className="bg-gray-400 min-h-[50px]">
+        <div className="bg-secondary p-2">
 
         </div>
       </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,11 +8,12 @@ interface NavbarProps {
 
 const Navbar = ({handleUpload, handleLogOut}: NavbarProps) => {
     return <div className="navbar justify-end bg-secondary shadow-sm space-x-2 px-6">
-        <button onClick={handleUpload} className="btn btn-square bg-transparent border-transparent hover:bg-primary group text-accent">
-            <UploadSVG className="group-hover:text-content"/>
+        <button onClick={handleUpload} className="btn bg-transparent border-transparent hover:bg-primary group text-muted hover:text-content">
+            <UploadSVG />
+            Upload
         </button>
-        <button onClick={handleLogOut} className="btn btn-square bg-transparent border-transparent hover:bg-primary group text-accent">
-            <LogOutSVG className="group-hover:text-red-500"/>
+        <button onClick={handleLogOut} className="btn btn-square bg-transparent border-transparent hover:bg-primary group">
+            <LogOutSVG className="text-muted group-hover:text-red-500"/>
         </button>
     </div>
 }
